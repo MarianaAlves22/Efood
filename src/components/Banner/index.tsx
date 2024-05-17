37893c5d31319b30b0b36ae9a051eec8dfca5ea3
Mariana Alves <Mary_A2903@hotmail.com>
2024-05-17 17:33:57 -0300

    corrigindo erros na digitação

--- src/components/Banner/index.tsx
+++ src/components/Banner/index.tsx
@@ -2,14 +2,14 @@ import banerImage from '../../assets/images/la_dolce.png'
 import { BannerContainer } from './styles'
 
 const Banner = () => (
-  <>
+  <div className="container">
     <BannerContainer style={{ backgroundImage: `url(${banerImage})` }}>
       <div className="container">
         <h3>Italiana</h3>
         <h1>La Dolce Vita Trattoria</h1>
       </div>
     </BannerContainer>
-  </>
+  </div>
 )
 
 export default Banner
